a6af057dec72a022d760d5e035e67480562d21f0
Valerio Salzano <vlr.slzn@gmail.com>
2023-02-23 17:44:26 +0100

    basic layout done

--- js/main.js
+++ js/main.js
@@ -21,26 +21,43 @@ submit.addEventListener ('click',
 
                 let price = distance * farePerKm;
 
-                const ticketDiscount = document.getElementById("ticketDiscount");
+                const ticketDiscount = document.getElementById("printDiscount");
 
                 // discount check
                 if (age == "underage") {
                     // 20% discount to underage
                     price -= price * underageDiscount;
-                    // ticketDiscount.innerHTML = (`Underage ${underageDiscount * 100}% discount applied.`);
-                    console.log(`Underage ${underageDiscount * 100}% discount applied.`)
+                    ticketDiscount.innerHTML = "Young";
+                    console.log(`Underage ${underageDiscount * 100}% discount applied.`);
                 } else if (age == "senior") {
                     // 40% discount to seniors
                     price -= price * seniorDiscount;
-                    // ticketDiscount.innerHTML = (`Senior ${seniorDiscount * 100}% discount applied.`);
-                    console.log(`Senior ${seniorDiscount * 100}% discount applied.`)
+                    ticketDiscount.innerHTML = "Senior";
+                    console.log(`Senior ${seniorDiscount * 100}% discount applied.`);
+                } else {
+                    ticketDiscount.innerHTML = "Regular"
                 }
 
                 // fixing price to 2 digits after decimal point
                 price = price.toFixed(2);
 
-                // document.getElementById("ticketPrice").innerHTML = `Final ticket price: ${price} &euro;`;
-                console.log(`Final ticket price: ${price} &euro;`)
+                document.getElementById("printPrice").innerHTML = `${price} &euro;`;
+                console.log(`Final ticket price: ${price} &euro;`);
+
+                let printName = document.getElementById("printName");
+                if (userName.value == ""){
+                    printName.innerHTML = "Unknown";
+                } else {
+                    printName.innerHTML = userName.value;
+                }
+                //results print
+                
+                document.getElementById("printWagon").innerHTML = (Math.floor(Math.random()*13) + 1);
+                document.getElementById("printCode").innerHTML = (Math.floor(Math.random()*10000) + 90000);
+
+
+                // results-container display
+                document.querySelector('.results.container').classList.remove('hidden');
             
             } else {
                 // userDistance validity test error
@@ -60,5 +77,7 @@ reset.addEventListener ('click',
         userAge.value = "";
         userDistance.value = "";
         userName.value = "";
+
+        document.querySelector('.results.container').classList.add('hidden')
     }
 )
